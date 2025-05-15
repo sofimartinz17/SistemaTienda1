14552299edea8809643cce1a15f33d4b9fbff83a
NATH228 <nathaly.portillo@catolica.edu.sv>
2025-05-15 10:20:25 -0600

    //pruu

--- SistemaTienda.Models/Categoria.cs
+++ SistemaTienda.Models/Categoria.cs
@@ -18,7 +18,7 @@ namespace SistemaTienda.Models
         public string Nombre { get; set; }
 
         [Required]
-        [Display(Name ="Ingrese orden ve visualizacion")] //pruueba
+        [Display(Name ="Ingrese orden ve visualizacion")] //pru
 
         public int Orden { get; set; }
     }
